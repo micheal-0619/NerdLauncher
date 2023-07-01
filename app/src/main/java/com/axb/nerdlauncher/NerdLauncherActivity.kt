e160439491d8305b33837aff0a1d024234b990c1
Michael_0619 <any_leon@163.com>
2023-07-01 14:31:25 +0800

    [activities]get activities

--- app/src/main/java/com/axb/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/axb/nerdlauncher/NerdLauncherActivity.kt
@@ -1,10 +1,15 @@
 package com.axb.nerdlauncher
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+
+private const val TAG = "NerdLauncherActivity"
 
 class NerdLauncherActivity : AppCompatActivity() {
 
@@ -17,5 +22,18 @@ class NerdLauncherActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.app_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        setupAdapter()
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun setupAdapter() {
+        /* 我们创建了一个操作设为ACTION_MAIN、 类别设为CATEGORY_LAUNCHER的隐式intent */
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        Log.d(TAG, "setupAdapter: Found ${activities.size} activities")
     }
 }
