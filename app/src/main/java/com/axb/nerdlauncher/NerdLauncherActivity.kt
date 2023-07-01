f47f96cfde8e3e6a103ecf625a2a1922013a8ae2
Michael_0619 <any_leon@163.com>
2023-07-01 14:18:21 +0800

    [RecyclerView]init RecyclerView and modify NerdLauncherActivity file name

---
+++ app/src/main/java/com/axb/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,21 @@
+package com.axb.nerdlauncher
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
