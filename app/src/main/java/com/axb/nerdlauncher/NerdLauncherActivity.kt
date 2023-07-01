e85c201c6a9853b1f52710c53d38b119d1972bc0
Michael_0619 <any_leon@163.com>
2023-07-01 15:05:28 +0800

    [Adapter][ViewHolder]adapter and view holder achieve. Obtain the list of activities

--- app/src/main/java/com/axb/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/axb/nerdlauncher/NerdLauncherActivity.kt
@@ -2,9 +2,14 @@ package com.axb.nerdlauncher
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -33,7 +38,51 @@ class NerdLauncherActivity : AppCompatActivity() {
         }
 
         val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
 
         Log.d(TAG, "setupAdapter: Found ${activities.size} activities")
+
+        recyclerView.adapter = ActivityAdapter(activities)//为RecyclerView设置adapter
+    }
+
+
+    private class ActivityHolder(itemsView: View) : RecyclerView.ViewHolder(itemsView) {
+        private val nameTextView = itemView as TextView
+        private lateinit var resolveInfo: ResolveInfo
+
+        fun bindActivity(resolveInfo: ResolveInfo) {
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+        }
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) :
+        RecyclerView.Adapter<ActivityHolder>() {
+        override fun onCreateViewHolder(container: ViewGroup, viewType: Int): ActivityHolder {
+            val layoutInflater = LayoutInflater.from(container.context)
+
+            /*
+            * 我们在onCreateViewHolder(...)里实例化android.R.layout.simple_list_item_1布局。
+            * simple_list_item_1布局内置在Android框架里， 因此， 这里没有使用R.layout， 而是用了android.R.layout。
+            * */
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, container, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return activities.size
+        }
     }
 }
